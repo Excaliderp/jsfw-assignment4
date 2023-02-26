f5017d900264ca279e71c034a3e90fe2a2efddeb
Excaliderp <timothy.karlsson@edu.cmeducations.se>
2023-02-26 12:28:53 +0100

    changed api url

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
-// import ArticleComponent from './Characters'
 
 function App() {
   const [character, setCharacter] = useState([]);
 
+  const steamLink = "https://store.steampowered.com/app/"
+
   const fetchCharacter = () => {
-    return fetch('https://jsonplaceholder.typicode.com/users')
+    return fetch('https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15')
       .then((response) => response.json())
       .then((data) => setCharacter(data))
   }
 
   useEffect(() => {
     fetchCharacter()
-
   }, [])
+
   console.log(character)
+
   return (
     <div className='App'>
-      <h1>Disney</h1>
-      {character.map((char, i) => (
-        <div key={i}>
-          <h2>{char.name}</h2>
-          <p>{char.email}</p>
+      <h1>Steam games under €5</h1>
+      {character.map((char, id) => (
+        <div className='gameCard' key={id}>
+          <h2 className='gameTitle'>{char.title}</h2>
+          <img src={char.thumb} />
+          <p className='price'>€ {char.salePrice}</p>
+          <a href={"https://store.steampowered.com/app/" + char.steamAppID} target="_blank">Link to Store Page</a>
         </div>
       ))}
     </div>
